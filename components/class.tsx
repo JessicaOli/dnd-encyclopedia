4f78d8a1c8731130945fc583fdfdf13635c3c647
jessicaoli <jequinha_olivenca@hotmail.com>
2023-09-29 18:31:47 +0100

    Added class descriptions

--- components/class.tsx
+++ components/class.tsx
@@ -1,18 +1,32 @@
 import { ClassInfo } from "@models/classInfo";
 import Image from 'next/image';
+import {descriptions} from '../constants/classDescriptions';
 
 export default function Class({ classInfo }: { classInfo: ClassInfo }) {
+
+    const classDescription = descriptions.find(description=>{
+        return description.className === classInfo.name;
+    });
+
+    console.log(classInfo.name);
+
     return (
         <>
             <div className="bg-black rounded-md w-full">
                 <div className="m-6">
                     <div className="text-center text-white text-2xl">{classInfo.name}</div>
-                    <Image
-                        src={'/assets/images/classes/' + classInfo.name + '.png'}
-                        alt='Class reference image'
-                        width={1000}
-                        height={0}
-                    ></Image>
+                    <div className="flex">
+                        <Image
+                            src={'/assets/images/classes/' + classInfo.name + '.png'}
+                            alt='Class reference image'
+                            width={800}
+                            height={0}
+                        ></Image>
+                        <div className="text-slate-200">
+                            {classDescription?.description}
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </>
